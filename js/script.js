1d63a7fea08853df7ba70d21f0d34aa3fe98ae46
Frankie <dracko@hotmail.co.uk>
2020-06-20 08:41:14 +0000

    mouseover added to script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$('#fading').mouseenter(function() {
+    $('#left').addClass('.background-gradient-left');
+});
+$('.container').mouseleave(function() {
+    $('#left').removeClass('.background-gradient-left');
+});
